fec6de0910f4bfd5d0b1b7a8b5cb6bea7f041d08
Josh Wills <jcwills369@gmail.com>
2023-07-28 14:56:52 +0100

    created wikinimous app, added bootstrap styling, did not cry this time

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  title = "#{Faker::Adjective.positive.capitalize} #{Faker::Nation.nationality} #{Faker::Hacker.noun.capitalize}"
+  content = "#{Faker::Lorem.sentence} #{Faker::Lorem.paragraph(sentence_count: 8)}"
+  new_article = Article.new({ title:, content: })
+  new_article.save
+end
